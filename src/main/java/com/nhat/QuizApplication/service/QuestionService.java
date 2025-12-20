37125e2f2b2e8999f0c7ac839807ed2e15692d36
Nguyen Nhat <nldnhat182006@gmail.com>
2025-12-20 09:55:40 +0700

    add getAllQuestions method

--- src/main/java/com/nhat/QuizApplication/service/QuestionService.java
+++ src/main/java/com/nhat/QuizApplication/service/QuestionService.java
@@ -1,9 +1,12 @@
 package com.nhat.QuizApplication.service;
 
 
+import com.nhat.QuizApplication.model.Question;
 import com.nhat.QuizApplication.repo.QuestionRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class QuestionService {
@@ -11,7 +14,7 @@ public class QuestionService {
     @Autowired
     private QuestionRepo questionRepo;
 
-    public String getAllQuestions() {
+    public List<Question> getAllQuestions() {
         return questionRepo.findAll();
     }
 }
