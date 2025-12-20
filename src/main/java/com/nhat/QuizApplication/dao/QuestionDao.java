6b7eedb8a73ee47740e0e0f5d51bbb8babbc7857
Nguyen Nhat <nldnhat182006@gmail.com>
2025-12-20 10:05:29 +0700

    database connection

--- src/main/java/com/nhat/QuizApplication/dao/QuestionDao.java
+++ src/main/java/com/nhat/QuizApplication/dao/QuestionDao.java
@@ -1,5 +1,9 @@
 package com.nhat.QuizApplication.dao;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class QuestionDao {
+@Repository
+public interface QuestionDao extends JpaRepository<QuestionDao, Integer> {
+
 }
