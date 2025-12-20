0ca54f9a5ff66a3dfb95a1e5c97d6d30ca5c8e2a
Nguyen Nhat <nldnhat182006@gmail.com>
2025-12-20 20:26:47 +0700

    add Response Entity

--- src/main/java/com/nhat/QuizApplication/controller/QuestionController.java
+++ src/main/java/com/nhat/QuizApplication/controller/QuestionController.java
@@ -5,6 +5,8 @@ package com.nhat.QuizApplication.controller;
 import com.nhat.QuizApplication.model.Question;
 import com.nhat.QuizApplication.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,17 +19,17 @@ public class QuestionController {
     private QuestionService questionService;
 
     @GetMapping("/allQuestions")
-    public List<Question> getAllQuestions() {
+    public ResponseEntity<List<Question>> getAllQuestions() {
         return questionService.getAllQuestions();
     }
 
     @GetMapping("/category/{category}")
-    public List<Question> getCategory(@PathVariable String category) {
+    public ResponseEntity<List<Question>> getCategory(@PathVariable String category) {
         return questionService.getQuestionsByCategory(category);
     }
 
     @PostMapping("/add")
-    public String addQuestion(@RequestBody Question question) {
+    public ResponseEntity<String> addQuestion(@RequestBody Question question) {
         return questionService.addQuestion(question);
     }
 
