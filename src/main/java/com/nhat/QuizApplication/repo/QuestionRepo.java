8cd016aae322148b29d82124185a2442c3a526ab
Nguyen Nhat <nldnhat182006@gmail.com>
2025-12-20 10:02:54 +0700

    delete repo package

--- src/main/java/com/nhat/QuizApplication/repo/QuestionRepo.java
+++
@@ -1,9 +0,0 @@
-package com.nhat.QuizApplication.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepo extends JpaRepository {
-
-}
