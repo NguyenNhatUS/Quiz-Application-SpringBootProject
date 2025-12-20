f797222c11927f7aeb98aa11c98bd1f5d4d9b325
Nguyen Nhat <nldnhat182006@gmail.com>
2025-12-20 09:03:52 +0700

    refresh Spring Boot

--- src/main/java/com/nhat/QuizApplication/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.nhat.QuizApplication;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Nguyen Le Duc Nhat");
-    }
-}
